789f0914372c4c2ca06ea3e321e57458cfbb3eaf
Bran <bran.whetter@gmail.com>
2017-07-16 19:20:19 +1000

    Update animatedGradientBG.js

--- animatedGradientBG.js
+++ animatedGradientBG.js
@@ -1,14 +1,10 @@
-
 var colors = new Array(
-  //[251,243,246],
-  //[200,220,208],
-  //[111,123,140],
-  //[244,226,156],
-  //[141,158,170]);
-[255,0,0],
-[0,255,0],
-[0,0,255],
-[0,0,0]);
+  [62,35,255],
+  [60,255,60],
+  [255,35,98],
+  [45,175,230],
+  [255,0,255],
+  [255,128,0]);
 
 var step = 0;
 //color table indices for: 
@@ -19,10 +15,13 @@ var step = 0;
 var colorIndices = [0,1,2,3];
 
 //transition speed
-var gradientSpeed = .015;
+var gradientSpeed = 0.002;
 
 function updateGradient()
 {
+  
+  if ( $===undefined ) return;
+  
 var c0_0 = colors[colorIndices[0]];
 var c0_1 = colors[colorIndices[1]];
 var c1_0 = colors[colorIndices[2]];
@@ -32,18 +31,16 @@ var istep = 1 - step;
 var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
 var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
 var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-var color1 = "#"+((r1 << 16) | (g1 << 8) | b1).toString(16);
+var color1 = "rgb("+r1+","+g1+","+b1+")";
 
 var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
 var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
 var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-var color2 = "#"+((r2 << 16) | (g2 << 8) | b2).toString(16);
+var color2 = "rgb("+r2+","+g2+","+b2+")";
 
  $('#gradient').css({
-   
-   
    background: "-webkit-radial-gradient(center, circle cover, "+color1+","+color2+")"});
-  
+     
   step += gradientSpeed;
   if ( step >= 1 )
   {
